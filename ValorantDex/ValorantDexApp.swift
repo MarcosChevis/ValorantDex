28a92f7fed60271bcd3361cd76b3cefbfcfc3e56
Marcos Chevis <marcoschevism@gmail.com>
2022-09-29 20:15:32 -0300

    Initial Commit

---
+++ ValorantDex/ValorantDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantDexApp.swift
+//  ValorantDex
+//
+//  Created by Marcos Chevis on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ValorantDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
